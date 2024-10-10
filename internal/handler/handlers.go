bbbca0d1a71f37be279d7c57c7ae0ea5e584ab6e
angellllk <angellsrp98@gmail.com>
2024-10-10 15:58:20 +0300

    updated handlers.go

--- internal/handler/handlers.go
+++ internal/handler/handlers.go
@@ -14,28 +14,19 @@ type TaskHandler struct {
 }
 
 func (h *TaskHandler) CreateTask(ctx *fiber.Ctx) error {
-	br := models.HTTPResponse{
-		Error:   true,
-		Message: "can't parse data",
-	}
-
 	var task models.Task
 
 	if errBP := ctx.BodyParser(&task); errBP != nil {
 		log.Printf("got error parsing body: %v\n", errBP)
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		return h.handleValidationError(ctx, errBP.Error())
 	}
 
 	if len(task.Title) == 0 {
-		br.Message = "can't have task title empty"
-		log.Printf("got error: %s", br.Message)
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		return h.handleValidationError(ctx, "can't have task title empty")
 	}
 
 	if len(task.Description) == 0 {
-		br.Message = "can't have task description empty"
-		log.Printf("got error: %s", br.Message)
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		return h.handleValidationError(ctx, "can't have task description empty")
 	}
 
 	task.ID = uuid.NewString()
@@ -43,8 +34,7 @@ func (h *TaskHandler) CreateTask(ctx *fiber.Ctx) error {
 
 	if err := h.Service.Create(task); err != nil {
 		log.Printf("got error: %v\n", err)
-		br.Message = "can't create task"
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		return h.handleValidationError(ctx, "can't create task")
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(models.HTTPResponse{
@@ -54,20 +44,14 @@ func (h *TaskHandler) CreateTask(ctx *fiber.Ctx) error {
 }
 
 func (h *TaskHandler) GetTasks(ctx *fiber.Ctx) error {
-	br := models.HTTPResponse{
-		Error:   true,
-		Message: "no tasks created",
-	}
-
 	tasks, err := h.Service.FetchAll()
 	if err != nil {
-		log.Printf("got error: %v\n", err)
-		br.Message = "can't fetch tasks"
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		log.Printf("got error: %v", err)
+		return h.handleValidationError(ctx, "can't fetch tasks")
 	}
 
 	if len(tasks) == 0 {
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		return h.handleValidationError(ctx, "no tasks found")
 	}
 
 	ret := models.TasksJSON{
@@ -79,53 +63,37 @@ func (h *TaskHandler) GetTasks(ctx *fiber.Ctx) error {
 }
 
 func (h *TaskHandler) GetTask(ctx *fiber.Ctx) error {
-	br := models.HTTPResponse{
-		Error:   true,
-		Message: "task not found",
-	}
-
 	id := ctx.Params("id")
 	task, err := h.Service.Fetch(id)
 	if err != nil {
 		log.Printf("got error: %v\n", err)
-		br.Message = "can't fetch task"
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		return h.handleValidationError(ctx, "can't fetch task")
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(task)
 }
 
 func (h *TaskHandler) UpdateTask(ctx *fiber.Ctx) error {
-	br := models.HTTPResponse{
-		Error:   true,
-		Message: "can't find task",
-	}
-
 	var updatedTask models.Task
 
 	errBP := ctx.BodyParser(&updatedTask)
 	if errBP != nil {
 		log.Printf("got error parsing body: %v\n", errBP)
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		return h.handleValidationError(ctx, "can't parse body")
 	}
 
 	if len(updatedTask.Title) == 0 {
-		br.Message = "can't have task title empty"
-		log.Printf("got error: %s", br.Message)
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		return h.handleValidationError(ctx, "can't have task title empty")
 	}
 
 	if len(updatedTask.Description) == 0 {
-		br.Message = "can't have task description empty"
-		log.Printf("got error: %s", br.Message)
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		return h.handleValidationError(ctx, "can't have task description empty")
 	}
 
 	id := ctx.Params("id")
 	if err := h.Service.Update(id, updatedTask); err != nil {
 		log.Printf("got error: %v\n", err)
-		br.Message = "can't update task"
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		return h.handleValidationError(ctx, "can't update task")
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(models.HTTPResponse{
@@ -135,20 +103,22 @@ func (h *TaskHandler) UpdateTask(ctx *fiber.Ctx) error {
 }
 
 func (h *TaskHandler) DeleteTask(ctx *fiber.Ctx) error {
-	br := models.HTTPResponse{
-		Error:   true,
-		Message: "can't find task",
-	}
-
 	id := ctx.Params("id")
 	if err := h.Service.Delete(id); err != nil {
 		log.Printf("got error: %v\n", err)
-		br.Message = "can't delete task"
-		return ctx.Status(fiber.StatusOK).JSON(br)
+		return h.handleValidationError(ctx, "can't delete task")
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(models.HTTPResponse{
 		Error:   false,
 		Message: "",
 	})
+}
+
+func (h *TaskHandler) handleValidationError(ctx *fiber.Ctx, message string) error {
+	br := models.HTTPResponse{
+		Error:   true,
+		Message: message,
+	}
+	return ctx.Status(fiber.StatusBadRequest).JSON(br)
 }
